a12d23580dc0f445184e637ff433102ca37b61ea
elakamarcus <n0m4d@hotmail.com>
2016-09-09 16:00:40 +0800

    Updates in code. Still APIError..

--- nexpose_tags_from_ip.rb
+++ nexpose_tags_from_ip.rb
@@ -2,8 +2,20 @@ require 'netaddr'
 require 'CSV'
 require 'nexpose'
 
-$nsc = Nexpose::Connection.new('nexposeServer', 'nxadmin', "nxadmin")
+# Copied from MrPaul -- login method, instead of hardcoding username and password.
+# https://github.com/misterpaul/NexposeRubyScripts/
+default_host = '10.2.7.80'
+default_port = 3780
+default_name = 'nxadmin'
+
+host = ask('Enter the server name (host) for Nexpose: ') { |q| q.default = default_host }
+user = ask('Enter your username: ') { |q| q.default = default_name }
+pass = ask('Enter your password: ') { |q| q.echo = '*' }
+$file = ask('Enter the csv $file to grab data from: ') { |q| q.default = default_file }
+
+$nsc = Nexpose::Connection.new(host, user, pass)
 $nsc.login
+at_exit { $nsc.logout }
 
 #Check if tag exist, return true||false
 def isExist(name)
@@ -31,14 +43,24 @@ def netcidr (addr1, name)
     #this will have to be generalised, depending on number of columns
     puts "CIDR = #{cidr}"
     puts "#{name} = #{cidr.first} -> #{cidr.last}"
-    criterion = Nexpose::Tag::Criterion.new('IP_RANGE', 'IN', [cidr.first,cidr.last])
+    criterion = Nexpose::Tag::Criterion.new('IP_RANGE', 'IN', [cidr.first, cidr.last])
     criteria = Nexpose::Tag::Criteria.new(criterion)
     #check if tag exist, to either create new or update existing.
     if isExist(name)
+        puts "Tag: #{name} exist. Loading tag."
         id = findTagID(name)
         tag = Nexpose::Tag.load($nsc, id)
+        if tag.search_criteria.criteria.map(&:value).flatten.uniq.include?(addr1)
+            puts "#{addr} already in #{tag.name}"
+        else
+            tag.search_criteria = criteria
+            tag.save($nsc)
+        end
     else
+        puts "Tag: #{name} does not exist. Creating tag."
         tag = Nexpose::Tag.new(name, Nexpose::Tag::Type::Generic::LOCATION)
+        tag.search_criteria = criteria
+        tag.save($nsc)
     end
     puts "Critera: #{criteria}"
 end
@@ -47,9 +69,25 @@ end
 def netrange (addr1, addr2, name)
     #this will have to be generalised, depending on number of columns
     puts "#{name} = #{addr1} -> #{addr2}"
-    criterion = Nexpose::Tag::Criterion.new('IP_RANGE', 'IN', [addr1,addr2])
+    criterion = Nexpose::Tag::Criterion.new('IP_RANGE', 'IN', [addr1, addr2])
     criteria = Nexpose::Tag::Criteria.new(criterion)
     puts "Critera: #{criteria}"
+    if isExist(name)
+        puts "Tag: #{name} exist. Loading tag."
+        id = findTagID(name)
+        tag = Nexpose::Tag.load($nsc, id)
+        if tag.search_criteria.criteria.map(&:value).flatten.uniq.include?(addr1)
+            puts "#{addr} already in #{tag.name}"
+        else
+            tag.search_criteria = criteria
+            tag.save($nsc)
+        end
+    else
+        puts "Tag: #{name} does not exist. Creating tag."
+        tag = Nexpose::Tag.new(name, Nexpose::Tag::Type::Generic::LOCATION)
+        tag.search_criteria = criteria
+        tag.save($nsc)
+    end
 end
 
 def testIP(addr)
@@ -64,7 +102,7 @@ rex2 = /\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}/
 end
 
 #read the headers into ... headers :-)
-headers = CSV.open('filename', 'r') { |a| a.first }
+headers = CSV.open($file, 'r') { |a| a.first }
 
 #check if too many columns. 3 is desirable.
 if headers[3]
@@ -80,7 +118,7 @@ puts "second: #{headers[1]}"
 puts "third: #{headers[2]}"
 =end
 
-CSV.foreach('filename', {:headers => true, :encoding => "ISO-8859-15:UTF-8"}) do |row|
+CSV.foreach($file, {:headers => true, :encoding => "ISO-8859-15:UTF-8"}) do |row|
 if testIP(row[headers[0]])
     #check if string contains "/"
     if row[headers[0]]["/"]
